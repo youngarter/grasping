58fce09f2e156b2752323bb9879e26fe4449126d
youngarter <youngesida@gmail.com>
2025-11-14 16:42:48 +0100

    first deploy

---
+++ app/(user)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        this is the dashboard fot the users
+      </div>
+    </div>
+  );
+}
